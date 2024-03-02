6d34a56c7ebc6d00c079bc07751a0a15e3563be8
Lara Scremin <lscreminmendes@gmail.com>
2024-03-02 17:01:23 -0300

    first commit

---
+++ src/Idade.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class Idade {
+    public static void main(String[] args) throws Exception {
+
+        int idade = Integer.parseInt(JOptionPane.showInputDialog(null, "teste"));
+
+        if (idade >= 60) {
+            JOptionPane.showMessageDialog(null, "Você é idoso");
+        } else if (idade >= 18) {
+            JOptionPane.showMessageDialog(null, "Você é adulto");
+        } else if (idade >= 12) {
+            JOptionPane.showMessageDialog(null, "Você é jovem");
+        } else {
+            JOptionPane.showMessageDialog(null, "Você é uma cirnaça");
+        }
+    }
+}
