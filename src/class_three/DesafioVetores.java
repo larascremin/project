caad8cc9c3e1ed00df5a49feba1e86daf43bf6b7
Lara Scremin <lscreminmendes@gmail.com>
2024-03-13 20:59:13 -0300

    desafio

--- src/class_three/DesafioVetores.java
+++ src/class_three/DesafioVetores.java
@@ -11,14 +11,20 @@ public class DesafioVetores {
         int[] alturas = new int[quantidade];
         String[] sexos = new String[quantidade];
         double media = 0;
+        double mediaGeral = 0;
         int somaAltura = 0;
+        int mulherAlturas = 0;
         int mulher = 0;
+        int pessoasAlturas = 0;
+        int pessoas = 0;
+        String mulheresAltas = "";
+        String pessoasBaixas = "";
 
         for (int i = 0; i < quantidade; i++) {
             nomes[i] = JOptionPane.showInputDialog(null, "Insira o nome: " + (i + 1));
             alturas[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira a altura de " + nomes[i]));
             sexos[i] = JOptionPane.showInputDialog(null,
-                    "Insira o sexo de " + nomes[i] + " utilizando m para masculino e f para feminino");
+                    "Insira o sexo de " + nomes[i] + " (F = feminino / M = masculino)");
             somaAltura += alturas[i];
         }
 
@@ -32,7 +38,43 @@ public class DesafioVetores {
             }
 
         }
+
         JOptionPane.showMessageDialog(null, "pessoa mais alta: " + maisAlto);
+
+        for (int i = 0; i < quantidade; i++) {
+            if (sexos[i].equalsIgnoreCase("f")) {
+                mulherAlturas += alturas[i];
+                mulher++;
+            }
+
+        }
+
+        media = mulherAlturas / mulher;
+
+        for (int i = 0; i < quantidade; i++) {
+            if (sexos[i].equalsIgnoreCase("f")) {
+                if (alturas[i] >= media) {
+                    mulheresAltas += nomes[i] + "; ";
+                }
+            }
+        }
+
+        for (int i = 0; i < quantidade; i++) {
+            pessoasAlturas += alturas[i];
+            pessoas++;
+
+        }
+
+        mediaGeral = pessoasAlturas / pessoas;
+
+        for (int i = 0; i < quantidade; i++) {
+            if (alturas[i] < mediaGeral) {
+                pessoasBaixas += nomes[i] + "; ";
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "mulehres altas: " + mulheresAltas);
+        JOptionPane.showMessageDialog(null, "pessoas baixas: " + pessoasBaixas);
 
     }
 
